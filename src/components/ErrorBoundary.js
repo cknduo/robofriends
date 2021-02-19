a1e3bf1fc592d7f64684d5edb3ddf978b8ecdc8a
cknduo <chloeesilvester@gmail.com>
2021-02-19 00:00:15 -0700

    small fix

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 
 class ErrorBoundary extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
             hasError = false
         }
     }
     
+    componentDidCatch(error, info) {
+        this.setState({ hasError: true })
+    }
+
     render() {
         if (this.state.hasError) {
             return <h1>Opps. You broke it.</h1>
